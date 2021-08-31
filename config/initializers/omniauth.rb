e2be4df635230b4515c3e2e79ac468eea02eeaac
souravongit <sourav@curvetomorrow.com.au>
2021-08-31 10:31:04 +0530

    first commit

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :keycloak_openid, 'allergypal-backend', '9f57eb2c-9298-4e19-84c4-5c3a01ce759f',
+#            client_options: {site: 'https://sso.resourcestack.com/auth/', realm: 'AllergyPal'}
+# end
